69cb35fca0f9758afef95a15049afed21804c12d
Zane Shelby <zane@users.noreply.github.com>
2018-09-24 22:49:11 -0400

    Move lein-jupyter plugin out of profile

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject metaprob "0.1.0-SNAPSHOT"
   :jvm-opts ["-Xss50M"] ; Default stack size is 1MB or less, increase to 50
-  :plugins [[lein-tools-deps "0.4.1"]]
+  :plugins [[lein-tools-deps "0.4.1"]
+            [lein-jupyter "0.1.16"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
-  :lein-tools-deps/config {:config-files [:install :user :project]}
-  :profiles {:jupyter {:plugins [[lein-jupyter "0.1.16"]]}})
+  :lein-tools-deps/config {:config-files [:install :user :project]})
