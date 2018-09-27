3207792f7f832efe06b5f8ca157c8bf63776e477
Zane Shelby <zane@users.noreply.github.com>
2018-09-26 23:12:53 -0400

    Remove changes that aren't strictly about Docker

--- bin/start_cider_nrepl.clj
+++
@@ -1,7 +0,0 @@
-(require '[cider-nrepl.main])
-
-(cider-nrepl.main/start-nrepl
- {:bind "0.0.0.0"
-  :port 9009
-  :middleware ["refactor-nrepl.middleware/wrap-refactor"
-               "cider.nrepl/cider-middleware"]})
