dc4410b995296b99c92b5a6164d59e397cfcf820
Zane Shelby <zane@users.noreply.github.com>
2018-10-08 16:15:08 -0400

    Fix map handling in free-vars-approxoimately

--- test/metaprob/syntax_test.clj
+++ test/metaprob/syntax_test.clj
@@ -87,3 +87,29 @@
         (eval form)
         (let [proc (eval 'foo)]
           (is (= (proc 17) 17) proc))))))
+
+(deftest free-vars-approximately-1
+  (testing "free-vars-approximately"
+    (testing "one level deep"
+      (are [vars form] (= vars (free-vars-approximately form))
+        '#{}    '[]
+        '#{a}   '[a]
+        '#{a}   (seq '[a])
+        '#{a b} '{a b}))
+    (testing "two levels deep"
+      (are [vars form] (= vars (free-vars-approximately form))
+        '#{a b}     '[a [b]]
+        '#{a b}     ['a (seq ['b])]
+        '#{a b}     (seq ['a (seq ['b])])
+        '#{a b c d} '[{a b} {c d}]
+        '#{a b c d} {'[a b] '[c d]}
+        '#{a b c d} {(seq '[a b]) (seq '[c d])}))
+    (testing "many levels deep"
+      (are [vars form] (= vars (free-vars-approximately form))
+        '#{a b c d e f g} '[a [b [c [d [e [f [g]]]]]]]
+        '#{a b c d}       (seq ['a (seq ['b (seq ['c (seq ['d])])])])))
+    (testing "filtering"
+      (are [vars form] (= vars (free-vars-approximately form))
+        '#{a b} '[a "filtered" b]
+        '#{a b} (seq '[a "filtered" b])
+        '#{a b} '{a ["filtered" b]}))))
