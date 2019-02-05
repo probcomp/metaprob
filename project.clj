4f54a1141a89fe4c9bbeaea544a5fe34a12343bd
Feras A Saad <fsaad@mit.edu>
2019-02-04 19:28:07 -0500

    Revert "Add lein exec to run scripts using lein exec `/path/to/script` easily."
    This reverts commit 7e3da05c4630500c653174230880171ad7bba71b.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :dependencies [[org.clojure/data.json "0.2.6"]
                  [lein-jupyter "0.1.16"]]
   :plugins [[lein-tools-deps "0.4.3"]
-            [lein-exec "0.3.7"]
             [lein-jupyter "0.1.16"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]})
