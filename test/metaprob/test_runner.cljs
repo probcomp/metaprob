de00e3fb31aee55740252948f62c4deb89afa47c
Zane Shelby <zane@users.noreply.github.com>
2019-03-29 15:16:45 -0400

    Require prelude

--- test/metaprob/test_runner.cljs
+++ test/metaprob/test_runner.cljs
@@ -1,7 +1,8 @@
 (ns metaprob.test-runner
   (:require [cljs.test :as test :include-macros true]
             [metaprob.code-handlers]
-            [metaprob.generative-functions]))
+            [metaprob.generative-functions]
+            [metaprob.prelude]))
 
 (defn -main
   [& args]
