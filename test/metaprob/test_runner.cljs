fa0d830d4387fc3aad4c98419f8c255d519c6053
Zane Shelby <zane@users.noreply.github.com>
2019-03-29 14:09:43 -0400

    Support ClojureScript compilation of metaprob.code-handlers

--- test/metaprob/test_runner.cljs
+++ test/metaprob/test_runner.cljs
@@ -1,5 +1,6 @@
 (ns metaprob.test-runner
-  (:require [cljs.test :as test :include-macros true]))
+  (:require [cljs.test :as test :include-macros true]
+            [metaprob.code-handlers]))
 
 (defn -main
   [& args]
