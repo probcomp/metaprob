a4be5c63b0b880a13e56dc602582c59d99f38d1d
Zane Shelby <zane@users.noreply.github.com>
2019-03-29 15:05:29 -0400

    Move make-generative-function to earlier compilation stage, test

--- test/metaprob/test_runner.cljs
+++ test/metaprob/test_runner.cljs
@@ -1,6 +1,7 @@
 (ns metaprob.test-runner
   (:require [cljs.test :as test :include-macros true]
-            [metaprob.code-handlers]))
+            [metaprob.code-handlers]
+            [metaprob.generative-functions]))
 
 (defn -main
   [& args]
