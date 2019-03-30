5f6ca269d3973b03549668689d73fb4da24c766b
Zane Shelby <zane@users.noreply.github.com>
2019-03-29 23:31:08 -0400

    Add ClojureScript compilation support for autotrace

--- test/metaprob/test_runner.cljs
+++ test/metaprob/test_runner.cljs
@@ -1,5 +1,6 @@
 (ns metaprob.test-runner
   (:require [cljs.test :as test :include-macros true]
+            [metaprob.autotrace]
             [metaprob.code-handlers]
             [metaprob.expander]))
 
