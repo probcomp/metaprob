b10d19e2c15568ccf0177ce253824f5d158efdc8
ulli <ulli@mit.edu>
2019-04-18 10:29:54 -0400

    Finalize porting of CGPM tests. Test's still throw errors.
    ... but I am not yet sure this is due to incorrect porting of tests or incorrect
    porting of cgpm.clj or multimixture_dsl.clj. Investigating...

--- test/metaprob/cgpm_test.clj
+++ test/metaprob/cgpm_test.clj
@@ -82,3 +82,211 @@
   (is (< (cgpm-logpdf dummy-cgpm {:x1 120} {:x0 2} {:y 100})))
   (is (< (cgpm-logpdf dummy-cgpm {:x0 2 :x1 120} {} {:y 100}))))
 
+(deftest dummy-row-simulate
+  (testing "dummy-row-simulate"
+    (let
+      [sample-1 (cgpm-simulate dummy-cgpm [:x0 :x1 :x2] {} {:y 100} 10)
+       sample-2 (cgpm-simulate
+                   dummy-cgpm
+                   [:x0 :x1 :x2 :x3]
+                   {:x3 "foo"}
+                   {:y 100} 20)]
+      (is (= (count sample-1)) 10)
+      (is (= (count sample-2) 20))
+      (is (= (get (nth sample-2 0) :x3) "foo")))))
+
+(deftest dummy-row-mutual-information-zero
+  (testing "dummy-row-mutual-information-zero"
+    (is (< (cgpm-mutual-information dummy-cgpm [:x0] [:x1] [] {:x3 "foo"}
+                                               {:y 100} 10 1))
+            1E-10)))
+
+(deftest dummy-row-kl-divergence-zero
+  (testing "dummy-row-kl-divergence-zero"
+  (is (< (cgpm-kl-divergence dummy-cgpm [:x0] [:x0] {} {:x3 "foo"} {:y 100} 10)
+         1E-10))))
+
+(deftest dummy-row-kl-divergence-non-zero
+  (testing "dummy-row-kl-divergence-non-zero"
+  (is (> (cgpm-kl-divergence dummy-cgpm [:x1] [:x2] {} {:x3 "foo"} {:y 100} 10)
+         1))))
+
+; --------------------------
+; TEST FOR MULTIMIXTURE CGPM
+; --------------------------
+
+(def generate-crosscat-row
+  (multi-mixture
+    ;; XXX: here, there is a change I don't fully understand -- Feras' multi-mixture takes a list of views,
+    ;; Alex's doesn't.
+    (view
+      {"sepal_width" gaussian
+        "petal_width" gaussian
+        "name" categorical
+        "sepal_length" gaussian
+        "petal_length" gaussian}
+       (clusters
+        0.325162847357 {"sepal_width" [34.179974 3.771946]
+                        "petal_width" [2.440002 1.061319]
+                        "name" [[0.986639 0.002751 0.002445]]
+                        "sepal_length" [50.060013 3.489470]
+                        "petal_length" [14.640005 1.717676]}
+        0.158811538073 {"sepal_width" [29.400019 2.727630]
+                        "petal_width" [14.599998 1.296148]
+                        "name" [[0.007976 0.972954 0.005166]]
+                        "sepal_length" [63.080017 3.877322]
+                        "petal_length" [45.879978 1.986359]}
+        0.152157485702 {"sepal_width" [30.666668 2.153812]
+                        "petal_width" [21.291676 2.423479]
+                        "name" [[0.007944 0.009516 0.968360]]
+                        "sepal_length" [65.874985 2.437782]
+                        "petal_length" [55.375000 2.429196]}
+        0.132195328587 {"sepal_width" [26.380953 2.256758]
+                        "petal_width" [11.952375 1.290116]
+                        "name" [[0.011187 0.958093 0.017295]]
+                        "sepal_length" [56.238064 2.327993]
+                        "petal_length" [39.714233 2.762729]}
+        0.0922710143592 {"sepal_width" [27.133326 2.124983]
+                         "petal_width" [18.266705 2.143722]
+                         "name" [[0.005149 0.009685 0.963620]]
+                         "sepal_length" [59.133389 3.461565]
+                         "petal_length" [49.599995 1.624789]}
+        0.0656548048737 {"sepal_width" [31.363638 3.960549]
+                         "petal_width" [20.909092 2.108620]
+                         "name" [[0.007970 0.016508 0.947206]]
+                         "sepal_length" [74.999992 2.558409]
+                         "petal_length" [63.454559 3.201247]}
+        0.0124223859026 {"sepal_width" [22.999990 2.160242]
+                         "petal_width" [10.333331 0.471404]
+                         "name" [[0.040683 0.858193 0.044297]]
+                         "sepal_length" [50.000000 0.816497]
+                         "petal_length" [32.666668 2.054805]}
+        0.0613245951451 {"sepal_width" [30.540000 4.321466]
+                         "petal_width" [11.986667 7.606126]
+                         "name" [[0.333333 0.333333 0.333333]]
+                         "sepal_length" [58.433333 8.253013]
+                         "petal_length" [37.586667 17.585292]}))))
+
+(def crosscat-cgpm
+  (let [outputs-addrs-types {; Variables in the table.
+                              :sepal_length real-type
+                              :sepal_width real-type
+                              :petal_length real-type
+                              :petal_width real-type
+                              :name integer-type
+                              ; Exposed latent variables.
+                              :cluster-for-sepal_length integer-type
+                              :cluster-for-sepal_width integer-type
+                              :cluster-for-petal_length integer-type
+                              :cluster-for-petal_width integer-type
+                              :cluster-for-name integer-type}
+        output-addr-map (make-identity-output-addr-map outputs-addrs-types)
+        inputs-addrs-types {}
+        input-addr-map {}]
+  (make-cgpm generate-crosscat-row
+             outputs-addrs-types
+             inputs-addrs-types
+             output-addr-map
+             input-addr-map)))
+
+; TODO: Provide coverage for the following cases:
+; 1. A MultiMixture CGPM with more than one view.
+
+; TODO: Write tests which capture assertion fails for simulate/logpdf errors:
+; 1. cluster-for-[varname] in same view contradict each other.
+
+(deftest crosscat-row-logpdf-agree-data
+  (testing "crosscat-row-logpdf-agree-data"
+    (let [lp-multimix (nth
+                        (infer-and-score
+                          :procedure generate-crosscat-row
+                          :observation-trace {"sepal_width" {:value 34}})
+                       2)
+          lp-cgpm (cgpm-logpdf crosscat-cgpm {:sepal_width 34} {} {})]
+    (is (< (relerr lp-cgpm lp-multimix) 1E-4)))))
+
+(deftest crosscat-row-logpdf-agree-cluster
+  (testing "crosscat-row-logpdf-agree-cluster"
+    (let [lp-multimix (nth
+                        (infer-and-score
+                          :procedure generate-crosscat-row
+                          :observation-trace {"cluster-for-sepal_length" {:value 0}})
+                        2)
+          lp-cgpm (cgpm-logpdf crosscat-cgpm
+                    {:cluster-for-sepal_length 0} {} {})]
+    (is (< (relerr lp-cgpm lp-multimix) 1E-4)))))
+
+(deftest crosscat-row-logpdf-agree-joint
+  (testing "crosscat-row-logpdf-agree-joint"
+    (let [lp-multimix (nth
+                        (infer-and-score
+                          :procedure generate-crosscat-row
+                          :observation-trace {"sepal_width" {:value 34}
+                                         "cluster-for-sepal_length" {:value 0}})
+                        2)
+          lp-cgpm (cgpm-logpdf
+                    crosscat-cgpm
+                    {:sepal_width 34 :cluster-for-sepal_length 0}
+                      {} {})]
+    (is (< (relerr lp-cgpm lp-multimix) 1E-4)))))
+
+(deftest crosscat-row-logpdf-conditional
+  (testing "crosscat-row-logpdf-conditional"
+    (let [lp-zx (cgpm-logpdf
+                  crosscat-cgpm
+                  {:sepal_width 34 :cluster-for-sepal_length 0}
+                  {} {})
+          lp-z (cgpm-logpdf
+            crosscat-cgpm
+              {:cluster-for-sepal_length 0}
+              {} {})
+          lp-x-given-z (cgpm-logpdf
+                          crosscat-cgpm
+                          {:sepal_width 34}
+                          {:cluster-for-sepal_length 0}
+                          {})
+          lp-x (cgpm-logpdf
+                 crosscat-cgpm
+                 {:sepal_width 34}
+                 {} {})
+          lp-z-given-x (cgpm-logpdf
+                         crosscat-cgpm
+                         {:cluster-for-sepal_length 0}
+                         {:sepal_width 34}
+                         {})]
+    (is (< (relerr lp-x-given-z (- lp-zx lp-z)) 1E-4))
+    (is (< (relerr lp-z-given-x (- lp-zx lp-x)) 1E-4)))))
+
+; TODO: Only run test under --slow option.
+(deftest crosscat-row-mi-nonzero
+  (testing "crosscat-row-mi-nonzero"
+    (let [mi (cgpm-mutual-information
+               crosscat-cgpm
+               [:sepal_length]
+               [:sepal_width]
+               {} {} {} 50 1)]
+    (is (> mi 1E-1)))))
+
+; TODO: Only run test under --slow option.
+(deftest crosscat-row-mi-conditional-indep-marginalize-z
+  (testing "crosscat-row-mi-conditional-indep-marginalize-z"
+    (let [mi (cgpm-mutual-information
+               crosscat-cgpm
+               [:sepal_length]
+               [:sepal_width]
+               {}
+               {:cluster-for-sepal_length 0}
+               {} 50 10)]
+    (is mi (< 1E-5)))))
+
+; TODO: Only run test under --slow option.
+(deftest crosscat-row-mi-conditional-indep-fixed-z
+  (testing "crosscat-row-mi-conditional-indep-fixed-z"
+    (let [mi (cgpm-mutual-information
+               crosscat-cgpm
+               [:sepal_length]
+               [:sepal_width]
+               [:cluster-for-sepal_length]
+               {}
+               {} 50 1)]
+    (is mi (< 1E-5)))))
